0f8d8d9b20dbf5fca864e43fee087ac1b452dcc3
Slowlife <slowlife1165@daum.net>
2021-08-05 12:40:37 +0700

    feat: auto commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import fetch from "node-fetch";
+import { Octokit } from "@octokit/rest";
 import fs from "fs";
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB,
+  userAgent: "SlowLife v1.0.0"
+});
 
 const beseURL = "https://discord.com/api/v9/users/374905512661221377";
 const file = fs.readFileSync("./README.md").toString("utf8");
@@ -14,7 +20,7 @@ const fetchUser = async () => {
       method: "GET",
       headers: {
         Authorization: process.env.DISCORD_TOKEN,
-      },
+      }
     })
   ).json();
 
@@ -26,7 +32,17 @@ fetchUser().then((user) => {
     matchedUsername,
     `${user.username}#${user.discriminator}`
   );
-  fs.writeFileSync("./README.md", replaced);
+
+  if (matchedUsername === `${user.username}#${user.discriminator}`)
+    return console.log("No action needed - username is still same.");
+
+  octokit.repos.createOrUpdateFileContents({
+    owner: "SlowLife1661",
+    repo: "SlowLife1661",
+    path: "./README.md",
+    content: replaced,
+    message: "chore: update username",
+  });
 
   console.log("All done!");
 });
