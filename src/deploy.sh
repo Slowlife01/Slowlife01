0de4f94e426bb01e39919b03bb2fa7f7a85d24df
Slowlife <slowlife1165@gmail.com>
2021-08-09 13:47:39 +0700

    refactor: better logs

--- src/deploy.sh
+++ src/deploy.sh
@@ -3,10 +3,14 @@
 set -eo pipefail
 cd $GITHUB_WORKSPACE
 
-echo "::[notice] # Commit and push"
+echo "[LOG] Executing bash script"
 
 git add .
 git config user.name "SlowLife1661"
 git config user.email "slowlife1165@gmail.com"
+
+echo "[LOG] Committing"
 git commit -m "chore: sync username" || true
+
+echo "[LOG] Pushing"
 git push origin "main"
