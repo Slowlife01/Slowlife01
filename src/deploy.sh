26a65748bd31a027706f217f3aaabef753f6a76c
Slowlife <slowlife1165@gmail.com>
2021-08-13 20:06:34 +0700

    refactor: change to `refactor`

--- src/deploy.sh
+++ src/deploy.sh
@@ -10,7 +10,7 @@ git config user.name "Slowlife01"
 git config user.email "slowlife1165@gmail.com"
 
 echo "[LOG] Committing"
-git commit -m "chore: sync username" || true
+git commit -m "refactor: sync username" || true
 
 echo "[LOG] Pushing"
 git push origin "main"
