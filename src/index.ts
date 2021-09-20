5ab5c4094eaf87fee5e6d88ca829752125c1d917
Slowlife <slowlife1165@gmail.com>
2021-09-20 12:19:13 +0700

    refactor(*)

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import fetch from "node-fetch";
 import fs from "fs";
 
 import { resolve } from "path";
-import { exec } from "@actions/exec";
 import { which } from "@actions/io";
+import { exec } from "@actions/exec";
 import { setFailed } from "@actions/core";
 
-const beseURL = "https://discord.com/api/v9/users/374905512661221377";
+const beseURL = "https://discord.com/api/v9";
 const file = fs.readFileSync("./README.md").toString("utf8");
 const matchedUsername = file.match(/([a-z]{2,32})[#][0-9]{4}/i)[0];
 
@@ -15,10 +15,10 @@ const fetchUser = async () => {
     username: string;
     discriminator: string;
   } = await (
-    await fetch(beseURL, {
+    await fetch(`${beseURL}/users/374905512661221377`, {
       method: "GET",
       headers: {
-        Authorization: process.env.DISCORD_TOKEN,
+        Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
       }
     })
   ).json();
@@ -27,12 +27,10 @@ const fetchUser = async () => {
 };
 
 fetchUser().then(async (user) => {
-  const replaced = file.replace(
-    matchedUsername,
-    `${user.username}#${user.discriminator}`
-  );
+  const username = `${user.username}#${user.discriminator}`;
+  const replaced = file.replace(matchedUsername, username);
 
-  if (matchedUsername === `${user.username}#${user.discriminator}`)
+  if (matchedUsername === username)
     return console.log("No action needed - username is still same.");
 
   fs.writeFile("./README.md", replaced, () => {
