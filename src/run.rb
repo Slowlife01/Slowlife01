8592cb53a6d5b3e6da1181da9b51e5dc3ec9d812
Slowlife <slowlife1165@gmail.com>
2021-12-30 13:41:52 +0700

    Update run.rb

--- src/run.rb
+++ src/run.rb
@@ -1,7 +1,9 @@
 require "httparty"
 require "json"
 
-file = File.read("./README.md")
+readmeFile = File.read("./README.md")
+contentFile = File.read("./PreMiD/content.txt")
+
 baseURL = "https://discord.com/api/v9"
 matched = file.match(/([a-z]{2,32})[#][0-9]{4}/i)[0]
 
@@ -13,14 +15,42 @@ define_method :fetchUser do
     return JSON.parse(response.body)
 end
 
+define_method :fetchContent do
+    response = HTTParty.get("http://api.github.com/graphql", 
+        :headers => {
+          "Authorization" => ENV["GITHUB"]
+        },
+        :body => %{
+          query {
+            repository(owner: "PreMiD", name: "Presences") {
+              discussion(4658) {
+                body
+              }
+            }
+          }
+        }
+    })
+
+    return JSON.parse(response.body)
+end
+
 user = fetchUser()
 username = user["username"] << "#" << user["discriminator"]
 
-replaced = file.gsub(matched, username)
+replaced = readme.gsub(matched, username)
 
 if (matched == username) 
     puts "No action needed - username is still the same."
 else
     File.write("./README.md", replaced)
+    exec(File.read(File.join(__dir__, "update.sh")))
+end
+
+content = fetchContent()
+
+if (content == contentFile)
+    puts "No action needed - content is still the same."
+else
+    File.write("./PreMiD/content.txt", content)
     exec(File.read(File.join(__dir__, "update.sh")))
 end
